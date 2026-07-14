4505bd3e6abba9c150eed6fcfcfba340a66ed516
ArshadShaikh107 <arshadshaikh70205@gmail.com>
2026-07-14 20:48:16 +0530

    Added dynamic dashboard with employee statistics

--- authenticate.php
+++ authenticate.php
@@ -23,29 +23,16 @@ if ($result->num_rows === 1) {
 
     $user = $result->fetch_assoc();
 
-    // DEBUG
-    echo "<pre>";
-    var_dump(password_verify($password, $user['password']));
-    echo "</pre>";
-
     if (password_verify($password, $user['password'])) {
 
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['username'] = $user['username'];
         $_SESSION['fullname'] = $user['fullname'];
 
-        echo "<pre>";
-        print_r($_SESSION);
-        echo "</pre>";
-
-        exit("Authentication Successful");
-
-    } else {
-
-        exit("Password Verify Failed");
-
+        header("Location: dashboard.php");
+        exit();
     }
-
 }
 
-exit("User Not Found");
+header("Location: login.php?error=1");
+exit();
