dc50990aa42241be463ecd901849886830ae4b33
ArshadShaikh107 <arshadshaikh70205@gmail.com>
2026-07-14 18:37:29 +0530

    Implemented secure user authentication

--- test_connection.php
+++ test_connection.php
@@ -1,5 +1,16 @@
 <?php
+
 require_once "config/db.php";
 
-echo "<h2>Database Connected Successfully ✅</h2>";
-?>
+echo "<h2>Database Information</h2>";
+
+echo "Host: " . $host . "<br>";
+echo "Database: " . $database . "<br><br>";
+
+$result = $conn->query("SELECT username, password FROM users");
+
+while ($row = $result->fetch_assoc()) {
+    echo "<pre>";
+    print_r($row);
+    echo "</pre>";
+}
