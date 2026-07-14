5fcdcf0c55e760eaf677283197bda7942588217c
ArshadShaikh107 <arshadshaikh70205@gmail.com>
2026-07-14 17:06:03 +0530

    Fix database connection

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
 
-$host = "mysql -h employee-db1.ch8qyagi69r7.ap-south-1.rds.amazonaws.com -P 3306 -u admin -p --ssl-mode=VERIFY_IDENTITY --ssl-ca=./global-bundle.pem";
+$host = "employee-db1.ch8qyagi69r7.ap-south-1.rds.amazonaws.com";
 $username = "admin";
 $password = "Arshad107";
 $database = "employee_management";
@@ -10,4 +10,7 @@ $conn = new mysqli($host, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection Failed: " . $conn->connect_error);
 }
+
+echo "Database Connected Successfully!";
+
 ?>
